65cf32d65300ffe6c3d125048d8d52dac97ea5be
khy00000 <lightram23@naver.com>
2025-07-18 13:59:28 +0900

    projects gsap 수정

---
+++ js/projects.js
@@ -0,0 +1,38 @@
+document.fonts.ready.then(() => {
+  const pptileSplit = new SplitText(".projects-p-title", {
+    type: "chars",
+    linesClass: "char",
+    mask: "chars",
+  });
+
+  const responsiveSplit = new SplitText(".projects-p-responsive", {
+    type: "chars",
+    linesClass: "char",
+    mask: "chars",
+  });
+
+  gsap.fromTo(
+    [pptileSplit.chars, responsiveSplit.chars],
+    { x: 100 },
+    {
+      x: 0,
+      duration: 1,
+      ease: "power1.out",
+    }
+  );
+
+  gsap.fromTo(
+    ".project-p-container a",
+    {
+      borderRadius: 0,
+    },
+    {
+      borderRadius: "50px",
+      duration: 2,
+      ease: "power2.out",
+      onComplete: () => {
+        document.querySelector(".toproject-p").style.opacity = 1;
+      },
+    }
+  );
+});
